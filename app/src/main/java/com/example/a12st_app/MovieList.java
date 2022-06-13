745f32f87804c8de5dab69a8c50b7e767277486c
khb03 <gpqls030@naver.com>
2022-06-13 22:23:34 +0900

    .

---
+++ app/src/main/java/com/example/a12st_app/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.a12st_app;
+
+public class MovieList {
+    MovieListResult boxOfficeResult;
+}
---
+++ app/src/main/java/com/example/a12st_app/MovieListResult.java
@@ -0,0 +1,11 @@
+package com.example.a12st_app;
+
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
